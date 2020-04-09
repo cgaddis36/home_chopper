c4ee5cc4417da620edc505aa9c1be3d67c41f711
Raymond Nguyen <rayknguyen@gmail.com>
2020-04-09 13:54:01 -0600

    Add user model Co-authored-by: Pierce Alworth <pbalworth@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
-    validates_presence_of :name
-    validates_presence_of :email
+# has_many :ingredients
+validates_presence_of :name
+validates_presence_of :email
 
-    has_many :ingredients
+  def self.from_omniauth(auth_info)
+    where(uid: auth_info[:uid]).first_or_create do |new_user|
+      new_user.uid                 = auth_info.uid
+      new_user.name                = auth_info.extra.raw_info.name
+      new_user.email               = auth_info.extra.raw_info.email
+      new_user.google_token        = auth_info.credentials.token
+    end
+  end 
 end
