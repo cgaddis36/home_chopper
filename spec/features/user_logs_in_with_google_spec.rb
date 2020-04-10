d1f1b1abd80bed50e9c62f652619177724b28961
Pierce Alworth <pbalworth@gmail.com>
2020-04-09 21:16:04 -0500

    Add oauth test. Mocked oauth hash

---
+++ spec/features/user_logs_in_with_google_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe 'user logs in' do 
+    scenario 'user logs in useer google oauth2' do
+        stub_omniauth 
+        visit '/'
+        
+        click_link 'Login with Google'
+
+        expect(page).to have_content("Jack Black")
+        expect(page).to have_link("Logout")
+    end 
+end 
+
+def stub_omniauth
+  OmniAuth.config.test_mode = true
+  OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+      uid: "12345678910",
+      info: {
+        name: "Jack Black",
+        email: "jack@nothing.com",
+      },
+      credentials: {
+        token: "abcdefg12345",
+        refresh_token: "12345abcdefg",
+        expires_at: DateTime.now
+      }
+  })
+end
