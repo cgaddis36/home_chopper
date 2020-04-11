34141f1da3d4dad79a9449afc0a25069e0476030
KellyIB <kellyibard@gmail.com>
2020-04-11 17:00:10 -0600

    add relationships to ingredients model

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,4 +2,7 @@ class Ingredient < ApplicationRecord
   validates_presence_of :name
 
   belongs_to :user
+  has_many :challenge_ingredients
+  has_many :challenges, through: :challenge_ingredients
+
 end
