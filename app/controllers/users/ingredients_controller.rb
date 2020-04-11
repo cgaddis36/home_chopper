91801542463a6137c4268dbd8950a2354ce90e64
KellyIB <kellyibard@gmail.com>
2020-04-10 22:03:27 -0600

    fix flash message and finish create method, add delete method
    Co-Authored-By: Chase Gaddis <cgaddis36@users.noreply.github.com>

--- app/controllers/users/ingredients_controller.rb
+++ app/controllers/users/ingredients_controller.rb
@@ -6,12 +6,24 @@ class Users::IngredientsController < ApplicationController
 
   def create
     user = User.find(params[:user_id])
-    if user.ingredients.create(ingredient_params)
+    new_ingredient = user.ingredients.new(ingredient_params)
+    if new_ingredient.save
+      flash[:success] = "New Item Saved"
       redirect_to "/users/#{user.id}/ingredients"
     else
       flash[:error] = "Name Can Not Be Blank"
+      redirect_to "/users/#{user.id}/ingredients"
     end
   end
+
+  def destroy
+    ingredient = Ingredient.find(params[:ingredient_id])
+    if ingredient.destroy
+      flash[:success] = "Ingredient Removed From Pantry"
+    end
+    redirect_to "/users/#{current_user.id}/ingredients"
+  end
+
 
 
   private
