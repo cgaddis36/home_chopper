f003c635dfc97e71b7ec243e4e97825feb551553
KellyIB <kellyibard@gmail.com>
2020-04-11 16:59:39 -0600

    add challenge model with relationships

---
+++ app/models/challenge.rb
@@ -0,0 +1,11 @@
+class Challenge < ApplicationRecord
+  validates_presence_of :time_limit
+  validates_presence_of :basket_size
+  validates_presence_of :meal_type
+  enum game_status: %i[before playing paused after]
+
+
+  belongs_to :user
+  has_many :challenge_ingredients
+  has_many :ingredients, through: :challenge_ingredients
+end
