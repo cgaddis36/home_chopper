e945d23316174c839a9b86e8b15d64fc204469f1
Chase.Gaddis <cgaddis36@gmail.com>
2020-04-11 10:53:17 -0500

    added namespaced user ingredients into routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#index'
   get '/auth/google_oauth2', as: :google_oauth2
-  get '/auth/google_oauth2/callback', to: 'sessions#create' 
+  get '/auth/google_oauth2/callback', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
+  namespace :users do
+    get '/:user_id/ingredients', to: 'ingredients#index'
+    post '/:user_id/ingredients', to: 'ingredients#create'
+    delete ':user_id/ingredients/:ingredient_id', to: 'ingredients#destroy'
+  end
   # devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 end
