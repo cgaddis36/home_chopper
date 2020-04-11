2eb44dbfde03df981e100114c937a18568954594
KellyIB <kellyibard@gmail.com>
2020-04-11 16:57:57 -0600

    add relatioinship tests to user and ingredient spec for new tables

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -7,5 +7,7 @@ RSpec.describe Ingredient, type: :model do
 
   describe 'relationships' do
     it { should belong_to :user }
+    it { should have_many :challenge_ingredients }
+    it {should have_many(:challenges).through(:challenge_ingredients)}
   end
 end
