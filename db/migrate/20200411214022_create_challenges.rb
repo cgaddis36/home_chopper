a093a0a122627d35a07854a151634cbf77391192
KellyIB <kellyibard@gmail.com>
2020-04-11 16:58:29 -0600

    create challenges table

---
+++ db/migrate/20200411214022_create_challenges.rb
@@ -0,0 +1,13 @@
+class CreateChallenges < ActiveRecord::Migration[5.2]
+  def change
+    create_table :challenges do |t|
+      t.integer :time_limit
+      t.integer :basket_size
+      t.integer :meal_type
+      t.integer :game_status, default: 0
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
