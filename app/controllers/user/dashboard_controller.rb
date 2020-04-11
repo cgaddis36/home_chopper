b19c70930c700856a3b23c07456571ecaca77439
Raymond Nguyen <rayknguyen@gmail.com>
2020-04-10 20:30:32 -0600

    Update file for test

--- app/controllers/user/dashboard_controller.rb
+++ app/controllers/user/dashboard_controller.rb
@@ -1,9 +1,5 @@
 class User::DashboardController < User::BaseController
   def index
-    if current_user
-      @user = current_user
-    else
-      redirect_to '/'
-    end
+    @user = current_user
   end
 end
