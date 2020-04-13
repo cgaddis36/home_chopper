ffcb817292d96c079825b95084ffee1bab2d673b
Raymond Nguyen <rayknguyen@gmail.com>
2020-04-13 15:57:23 -0600

    Adds application js for countdown

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,36 +15,3 @@
 //= require turbolinks
 //= require_tree .
 
-
-var submitButton = document.querySelector("#submitButton")
-
-submitButton.addEventListener('click', () => {
-
-  var num = document.querySelector("#inputNumber").value
-  var personInput = (Number(num) * 60) * 1000
-
-  var deadline = new Date(Date.now() + personInput).getTime();
-  var x = setInterval(function () {
-
-    var now = new Date().getTime();
-
-    var t = deadline - now;
-
-    var hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((t % (1000 * 60)) / 1000);
-
-    document.getElementById("hour").innerHTML = hours;
-    document.getElementById("minute").innerHTML = minutes;
-    document.getElementById("second").innerHTML = seconds;
-    if (t < 0) {
-      clearInterval(x);
-
-      document.getElementById("demo").innerHTML = "TIME'S UP";
-      document.getElementById("hour").innerHTML = '0';
-      document.getElementById("minute").innerHTML = '0';
-      document.getElementById("second").innerHTML = '0';
-    }
-  }, 1000);
-
-})
