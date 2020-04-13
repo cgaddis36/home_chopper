5a3954b09de28cb6479e12466e6679ef75ba4979
KellyIB <kellyibard@gmail.com>
2020-04-13 02:16:07 -0600

    moved folder into features from users

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -35,13 +35,11 @@ RSpec.configure do |config|
   config.after(:each) do
     DatabaseCleaner.clean
   end
-  
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
 
   config.expect_with :rspec do |expectations|
-
     # This option will default to `true` in RSpec 4. It makes the `description`
     # and `failure_message` of custom matchers include text for helper methods
     # defined using `chain`, e.g.:
