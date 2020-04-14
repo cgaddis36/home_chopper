1c3be9750ce4d4f2373c446fcc1d3639b6148c89
Chase.Gaddis <cgaddis36@gmail.com>
2020-04-14 16:29:29 -0500

    add photo string to migration and active storage files

--- db/migrate/20200411214022_create_challenges.rb
+++ db/migrate/20200411214022_create_challenges.rb
@@ -3,6 +3,7 @@ class CreateChallenges < ActiveRecord::Migration[5.2]
     create_table :challenges do |t|
       t.integer :time_limit
       t.integer :basket_size
+      t.string :photo
       t.integer :meal_type, default: 0
       t.integer :game_status, default: 0
       t.references :user, foreign_key: true
