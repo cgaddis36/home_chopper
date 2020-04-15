a5c22449a43abe70efa679c78456e0ee0af9903a
KellyIB <kellyibard@gmail.com>
2020-04-15 12:18:05 -0600

    add testing for index to show 7 ingredient challenges

--- spec/features/user/challenges/index_spec.rb
+++ spec/features/user/challenges/index_spec.rb
@@ -25,6 +25,14 @@ RSpec.describe 'User ' do
     ChallengeIngredient.create!(challenge_id: @jans_lunch.id, ingredient_id: @peanut_butter.id)
     ChallengeIngredient.create!(challenge_id: @jans_lunch.id, ingredient_id: @avacado.id)
     ChallengeIngredient.create!(challenge_id: @jans_lunch.id, ingredient_id: @lemon.id)
+    @jans_tuesday_lunch = @janis.challenges.create!(time_limit: 20, basket_size: 7, meal_type: "lunch")
+    ChallengeIngredient.create!(challenge_id: @jans_tuesday_lunch.id, ingredient_id: @quail_eggs.id)
+    ChallengeIngredient.create!(challenge_id: @jans_tuesday_lunch.id, ingredient_id: @sprite.id)
+    ChallengeIngredient.create!(challenge_id: @jans_tuesday_lunch.id, ingredient_id: @peanut_butter.id)
+    ChallengeIngredient.create!(challenge_id: @jans_tuesday_lunch.id, ingredient_id: @avacado.id)
+    ChallengeIngredient.create!(challenge_id: @jans_tuesday_lunch.id, ingredient_id: @lemon.id)
+    ChallengeIngredient.create!(challenge_id: @jans_tuesday_lunch.id, ingredient_id: @chocolate.id)
+    ChallengeIngredient.create!(challenge_id: @jans_tuesday_lunch.id, ingredient_id: @cinnamon.id)
     @jans_dinner = @janis.challenges.create!(time_limit: 20, basket_size: 3, meal_type: "dinner")
     ChallengeIngredient.create!(challenge_id: @jans_dinner.id, ingredient_id: @squid.id)
     ChallengeIngredient.create!(challenge_id: @jans_dinner.id, ingredient_id: @blueberries.id)
@@ -35,11 +43,21 @@ RSpec.describe 'User ' do
     ChallengeIngredient.create!(challenge_id: @jans_snack.id, ingredient_id: @eggs.id)
     ChallengeIngredient.create!(challenge_id: @jans_snack.id, ingredient_id: @toast.id)
     ChallengeIngredient.create!(challenge_id: @jans_snack.id, ingredient_id: @blueberries.id)
+    @jans_other_snack = @janis.challenges.create!(time_limit: 20, basket_size: 7, meal_type: "snack")
+    ChallengeIngredient.create!(challenge_id: @jans_other_snack.id, ingredient_id: @chocolate.id)
+    ChallengeIngredient.create!(challenge_id: @jans_other_snack.id, ingredient_id: @cinnamon.id)
+    ChallengeIngredient.create!(challenge_id: @jans_other_snack.id, ingredient_id: @eggs.id)
+    ChallengeIngredient.create!(challenge_id: @jans_other_snack.id, ingredient_id: @toast.id)
+    ChallengeIngredient.create!(challenge_id: @jans_other_snack.id, ingredient_id: @blueberries.id)
+    ChallengeIngredient.create!(challenge_id: @jans_other_snack.id, ingredient_id: @lemon.id)
+    ChallengeIngredient.create!(challenge_id: @jans_other_snack.id, ingredient_id: @avacado.id)
     @jans_second_breakfast = @janis.challenges.create!(time_limit: 20, basket_size: 3, meal_type: "breakfast")
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@janis)
   end
 
   it 'can see all the games that have been played' do
+    @jans_other_snack.game_complete
+    @jans_other_snack.reload
     @jans_snack.game_complete
     @jans_snack.reload
     @jans_dinner.game_complete
@@ -48,12 +66,16 @@ RSpec.describe 'User ' do
     @jans_breakfast.reload
     @jans_lunch.game_complete
     @jans_lunch.reload
+    @jans_tuesday_lunch.game_complete
+    @jans_tuesday_lunch.reload
 
     expect(@jans_second_breakfast.game_status).to eq("before")
     expect(@jans_snack.game_status).to eq("complete")
+    expect(@jans_other_snack.game_status).to eq("complete")
     expect(@jans_dinner.game_status).to eq("complete")
     expect(@jans_breakfast.game_status).to eq("complete")
     expect(@jans_lunch.game_status).to eq("complete")
+    expect(@jans_tuesday_lunch.game_status).to eq("complete")
 
     visit "/users/challenges"
 
@@ -101,6 +123,34 @@ RSpec.describe 'User ' do
         expect(page).to have_content(@eggs.name)
         expect(page).to have_content(@toast.name)
         expect(page).to have_content(@blueberries.name)
+        expect(page).to have_content("#{@jans_snack.time_limit} minutes")
+      end
+    end
+
+    within "#7ingredients" do
+      within "#challenge-#{@jans_tuesday_lunch.id}" do
+        expect(page).to have_content("Basket Size: #{@jans_tuesday_lunch.basket_size}")
+        expect(page).to have_button(@jans_tuesday_lunch.id)
+        expect(page).to have_content(@quail_eggs.name)
+        expect(page).to have_content(@sprite.name)
+        expect(page).to have_content(@lemon.name)
+        expect(page).to have_content(@avacado.name)
+        expect(page).to have_content(@peanut_butter.name)
+        expect(page).to have_content(@chocolate.name)
+        expect(page).to have_content(@cinnamon.name)
+        expect(page).to have_content("#{@jans_lunch.time_limit} minutes")
+      end
+
+      within "#challenge-#{@jans_other_snack.id}" do
+        expect(page).to have_content("Basket Size: #{@jans_other_snack.basket_size}")
+        expect(page).to have_button(@jans_other_snack.id)
+        expect(page).to have_content(@chocolate.name)
+        expect(page).to have_content(@cinnamon.name)
+        expect(page).to have_content(@eggs.name)
+        expect(page).to have_content(@toast.name)
+        expect(page).to have_content(@blueberries.name)
+        expect(page).to have_content(@lemon.name)
+        expect(page).to have_content(@avacado.name)
         expect(page).to have_content("#{@jans_snack.time_limit} minutes")
       end
     end
