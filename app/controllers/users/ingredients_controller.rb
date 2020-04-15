d34cc18b1842afdde6c3220a2cf117043a334674
KellyIB <kellyibard@gmail.com>
2020-04-14 23:52:37 -0600

    change hand-rolled routes to resources

--- app/controllers/users/ingredients_controller.rb
+++ app/controllers/users/ingredients_controller.rb
@@ -2,24 +2,24 @@
 
 class Users::IngredientsController < Users::BaseController
   def index
-    @ingredients = User.find(params[:user_id]).ingredients
+    @ingredients = User.find(current_user.id).ingredients
   end
 
   def create
-    user = User.find(params[:user_id])
+    user = User.find(current_user.id)
     new_ingredient = user.ingredients.new(ingredient_params)
     if new_ingredient.save
       flash[:success] = "New Item Saved"
     else
       flash[:error] = "Name Can Not Be Blank"
     end
-    redirect_to "/users/#{user.id}/ingredients"
+    redirect_to "/users/ingredients"
   end
 
   def destroy
-    ingredient = Ingredient.find(params[:ingredient_id])
+    ingredient = Ingredient.find(params[:id])
     flash[:success] = "Ingredient Removed From Pantry" if ingredient.destroy
-    redirect_to "/users/#{current_user.id}/ingredients"
+    redirect_to "/users/ingredients"
   end
 
   private
