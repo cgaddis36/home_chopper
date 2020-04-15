ef85199081180aa047426750fb74dcd36f44f219
Raymond Nguyen <rayknguyen@gmail.com>
2020-04-14 20:17:37 -0600

    Update to redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,4 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
-
-  def game_start
-    false
-  end
 end
