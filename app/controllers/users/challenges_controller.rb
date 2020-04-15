40debf919e047019ae616bd53f11173345574c8d
Chase.Gaddis <cgaddis36@gmail.com>
2020-04-15 16:10:53 -0500

    removed comment

--- app/controllers/users/challenges_controller.rb
+++ app/controllers/users/challenges_controller.rb
@@ -34,9 +34,6 @@ class Users::ChallengesController < Users::BaseController
       @challenge.cancel_game
     elsif params[:game_event] == "done"
       @challenge.finalize_game
-    elsif params[:game_event] == "save_photo"
-      # needs photo functionality added - active storage??
-      # @challenge.game_complete
     elsif params[:game_event] == "no_photo"
       @challenge.game_complete
     end
