6baa73dbf23ddb3dab9d20692655ac1218fb3b4f
Chase.Gaddis <cgaddis36@gmail.com>
2020-04-16 11:17:34 -0500

    changed dashboard route

--- config/routes.rb
+++ config/routes.rb
@@ -5,20 +5,8 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
   resources :challenges, only: [:index, :show]
 
-  namespace :users do
-    get '/:user_id/dashboard', to: 'dashboard#index'
-    get '/:user_id/ingredients', to: 'ingredients#index'
-    post '/:user_id/ingredients', to: 'ingredients#create'
-    delete '/:user_id/ingredients/:ingredient_id', to: 'ingredients#destroy'
-    get '/:user_id/challenges/new', to: 'challenges#new'
-    post '/:user_id/challenges', to: 'challenges#create'
-    get '/:user_id/challenges', to: 'challenges#index'
-    get '/:user_id/challenges/:challenge_id', to: 'challenges#show'
-    patch '/:user_id/challenges/:challenge_id/update', to: 'challenges#update'
-    post '/:user_id/ratings', to: 'ratings#create'
-  end
-
   scope :users, module: :users do
+    resources :dashboard
     resources :ingredients, except: [:new, :show, :edit]
     resources :challenges, except: :destroy
     resources :ratings, only: :create
