c233bb58261c5ff907f6263be9c02acf913a204e
KellyIB <kellyibard@gmail.com>
2020-04-16 17:09:06 -0600

    add to recipe facade
    Co-Authored-By: Pierce A (Him/He, BE) <palworth@users.noreply.github.com>

--- app/facades/recipe_search.rb
+++ app/facades/recipe_search.rb
@@ -9,12 +9,16 @@ class RecipeSearch
     @recipe_json ||= @service.get_recipes(@ingredients)
     recipes = []
     @recipe_json.each do |recipe_data|
-      recipes << Recipe.new(recipe_data)
+      recipes << Recipe.new(recipe_data, self)
     end
     recipes
   end
-  #
+
   # def get_recipe_instructions(id)
-  #   @recipe_instructions_json ||= @service.get_recipes(@ingredients)
+  #   @recipe_instructions_json ||= @service.get_recipe_instructions(id)
   # end
+
+  def get_recipe_information(id)
+    @recipe_ingredients_json ||= @service.get_recipe_info(id)
+  end
 end
