cd72f5fd807136920daef11acee0936ee8894afb
KellyIB <kellyibard@gmail.com>
2020-04-15 18:50:57 -0600

    rubocop cleanup

--- app/controllers/users/games_controller.rb
+++ app/controllers/users/games_controller.rb
@@ -1,9 +1,11 @@
-class Users::GamesController < ApplicationController
-  def show 
+# rubocop:disable Style/ClassAndModuleChildren
 
+class Users::GamesController < ApplicationController
+  def show
   end
 
   def hints
-    
   end
 end
+
+# rubocop:enable Style/ClassAndModuleChildren
