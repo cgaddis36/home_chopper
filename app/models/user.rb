a1ce5cd006e695259d6ea5a546223a9b2faf8ec1
Chase.Gaddis <cgaddis36@gmail.com>
2020-04-16 17:30:18 -0500

    changes to creating a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
   has_many :challenge_ingredients, through: :challenges
 
   def self.update_or_create(auth_info)
-    user = User.find_by(uid: auth_info[:uid]) || User.new
-    user.attributes = { uid: auth_info[:uid],
+    user = User.find_by(uid: auth_info[:uid]) || User.new(user_params)
+    user_params = { uid: auth_info[:uid],
                         email: auth_info[:info][:email],
                         name: auth_info[:info][:name],
                         google_token: auth_info[:credentials][:token] }
