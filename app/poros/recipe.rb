b7d452c41d3c704eb7c2dfb160cd4f4f2191409d
KellyIB <kellyibard@gmail.com>
2020-04-16 17:08:55 -0600

    add recipe poro
    Co-Authored-By: Pierce A (Him/He, BE) <palworth@users.noreply.github.com>

--- app/poros/recipe.rb
+++ app/poros/recipe.rb
@@ -1,14 +1,23 @@
 class Recipe
-  attr_reader :ingredients, :title
+  attr_reader :ingredients, :title, :instructions
 
-  def initialize(data)
+  def initialize(data, search_facade)
+    @search_facade = search_facade
     @title = data[:title]
-    @ingredients = ingredient_pull(data)
+    @ingredients = ingredients_pull(data[:id])
+    @instructions = instructions_pull(data[:id])
   end
 
-  def ingredient_pull(data)
-    data[:missedIngredients].map do |ingredient|
-      ingredient[:name]
+  def ingredients_pull(id)
+    recipe_data = @search_facade.get_recipe_information(id)
+    recipe_data[:extendedIngredients].map do |ingredient|
+      ingredient[:originalString]
     end
   end
+
+  def instructions_pull(id)
+    recipe_data = @search_facade.get_recipe_information(id)
+    recipe_data[:instructions]
+  end
+
 end
