a56ca32fd3c9e000fc81f5d7e8b8727cdab25733
KellyIB <kellyibard@gmail.com>
2020-04-17 02:06:08 -0600

    add status to ingredients

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_15_183909) do
+ActiveRecord::Schema.define(version: 2020_04_17_075035) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2020_04_15_183909) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.integer "status", default: 0
     t.index ["user_id"], name: "index_ingredients_on_user_id"
   end
 
