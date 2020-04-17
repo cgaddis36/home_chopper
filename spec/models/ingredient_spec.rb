9399a5fa70d6b40e00960e94b7252c3c4691a769
KellyIB <kellyibard@gmail.com>
2020-04-17 01:19:59 -0600

    add model test for new model method

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -10,4 +10,22 @@ RSpec.describe Ingredient, type: :model do
     it { should have_many :challenge_ingredients }
     it { should have_many(:challenges).through(:challenge_ingredients) }
   end
+
+  describe 'class methods' do
+    before(:each) do
+      @janis = User.create!(name: "Janice", email: "janice@sample.com", google_token: "678910", role: 0)
+      @jelly = @janis.ingredients.create!(name: "Jelly")
+      @quail_eggs = @janis.ingredients.create!(name: "Quail Eggs")
+      @pears = @janis.ingredients.create!(name: "Pears")
+      @jans_dessert = @janis.challenges.create!(time_limit: 20, basket_size: 3, meal_type: "lunch", game_status: "complete")
+      @challenge_ingredient = ChallengeIngredient.create!(challenge_id: @jans_dessert.id, ingredient_id: @pears.id)
+    end
+
+    it "can delete a challenge ingredient" do
+      expect(ChallengeIngredient.where("ingredient_id = #{@pears.id}")).to eq([@challenge_ingredient])
+      @pears.kill_join_entry
+      @pears.reload
+      expect(ChallengeIngredient.where("ingredient_id = #{@pears.id}")).to eq([])
+    end
+  end
 end
