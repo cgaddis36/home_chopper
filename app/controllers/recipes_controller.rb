5e15426bdb9deba77ea225d376bcc70c71b1e7d7
KellyIB <kellyibard@gmail.com>
2020-04-16 22:38:56 -0600

    wMerge branch 'dev' of github.com:cgaddis36/home_chopper into recipes-dev-pull

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,8 @@
 class RecipesController < ApplicationController
   def index
-    binding.pry
-    ingredients = params[:ingredient_1] + "," + params[:ingredient_2] + "," + params[:ingredient_3]
+    ingredients = (params[:ingredient_1] + "," + params[:ingredient_2] + "," +
+    params[:ingredient_3] + "," + params[:ingredient_4] + "," +
+    params[:ingredient_5] + "," + params[:ingredient_6] + "," + params[:ingredient_7])
     search = RecipeSearch.new(ingredients)
     @recipes = search.get_recipe_names
   end
